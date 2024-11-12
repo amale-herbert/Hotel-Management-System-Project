a58400c3740e4ab5268557470856a46a5ac646c9
Amale-h <2023akcs3544f@kab.ac.ug>
2024-11-12 23:55:06 +0300

    login.php

--- login.php
+++ login.php
@@ -18,7 +18,7 @@
     </nav>
     <div class="container">
         <h1>Login</h1>
-        <form action="login.php" method="post">
+        <form action="signin_db.php" method="post">
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
             <button type="submit">Login</button>
